e25e06c90ba7a263af27e072824f414c3bfefb16
derrick b <dbozich1@gmail.com>
2018-05-31 07:14:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,11 @@ class Artist
     @songs << song
   end
 
-  def add_song_by_name(song)
-    song = Song.new
-    song.artist = self
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
+    song.artist = self
+    
 
   end
 
