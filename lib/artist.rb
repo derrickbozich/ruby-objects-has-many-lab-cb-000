8eee05cd307d576425730dd5233e3ae0c20125a0
derrick b <dbozich1@gmail.com>
2018-05-31 07:07:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,22 +15,6 @@ class Artist
     song.artist = self
   end
 
-  
-
-end
-
-class Song
-  attr_accessor :name, :artist
-
-  @@all = []
-
-  def initialize(name)
-    @title = title
-  end
-
-  def self.all
-    @@all
-  end
 
 
 end
